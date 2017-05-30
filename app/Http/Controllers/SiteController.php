30e2a55ab108ae1649e39fa8cecbc3d984f9bf89
apekin-pc\apekin <apekin@uamaster.com>
2017-05-30 11:02:07 +0300

    less(5)

--- app/Http/Controllers/SiteController.php
+++ app/Http/Controllers/SiteController.php
@@ -4,6 +4,7 @@ namespace Corp\Http\Controllers;
 
 use Corp\Repositories\MenusRepository;
 use Illuminate\Http\Request;
+use Menu;
 
 class SiteController extends Controller
 {
@@ -30,7 +31,7 @@ class SiteController extends Controller
         $menu = $this->getMenu();
 
 
-        $navigation = view(env('THEME').'.navigation')->render();
+        $navigation = view(env('THEME').'.navigation')->with('menu',$menu)->render();
         $this->vars['navigation'] = $navigation;
 
         return view($this->template)->with($this->vars);
@@ -39,6 +40,18 @@ class SiteController extends Controller
     protected function getMenu(){
         $menu = $this->m_rep->get();
 
-        return $menu;
+        $mBuilder = Menu::make('MyNav', function($m) use ($menu) {
+           foreach($menu as $item){
+                if($item->parent == 0){
+                    $m->add($item->title, $item->path)->id($item->id);
+                }else{
+                    if($m->find($item->parent)){
+                        $m->find($item->parent)->add($item->title, $item->path)->id();
+                    }
+                }
+           }
+        });
+
+        return $mBuilder;
     }
 }
